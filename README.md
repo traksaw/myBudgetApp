74024a6d95f1efc544c5c888b4fef30b53ad034e
Waskar Paulino <155832641+traksaw@users.noreply.github.com>
2024-11-18 23:49:31 -0500

    v1.1

---
+++ README.md
@@ -0,0 +1,15 @@
+## Installation
+
+1. Clone repo
+2. run `npm install`
+
+## Usage
+
+1. run `node server.js`
+2. Navigate to `localhost:8080`
+
+## Credit
+
+Modified from Scotch.io's auth tutorial
+# myBudgetApp
+# myBudgetApp
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+                
+    'url' : 'mongodb+srv://waskarpaulino:Hello@cluster0.wabar.mongodb.net/budgetsDB?retryWrites=true&w=majority&appName=Cluster0', 
+    'dbName': 'budgetsDB'
+};
